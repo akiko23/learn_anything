33e5decded17a8ce0b7f359e7bd8f54570d83322
akiko23 <dmvasilenko43@gmail.com>
2024-12-31 14:03:49 +0300

    add archive containing vm images

--- scripts/init_playground_host.sh
+++ scripts/init_playground_host.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-mkdir -p /etc/learn_anything/qemu_disk_images
-cp ./images/debian12-2_python_base.qcow2 /etc/learn_anything/qemu_disk_images
+VM_IMAGES_BASE_PATH=/etc/learn_anything/qemu_disk_images
+
+
+mkdir -p $VM_IMAGES_BASE_PATH
+
+tar -xvzf vm_images/vm_images.tar.gz -C $VM_IMAGES_BASE_PATH
+mv $VM_IMAGES_BASE_PATH/images/* $VM_IMAGES_BASE_PATH && rm -r $VM_IMAGES_BASE_PATH/images
+
 cp -r ./scripts/ /etc/learn_anything/
 chmod ug+x -R /etc/learn_anything/scripts/
